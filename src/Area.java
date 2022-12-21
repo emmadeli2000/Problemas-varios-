850d32bb6eedcb94ecf888fe1e16d43f4473cf38
Emmanuel Delicia <emmanuel_delicia@hotmail.com>
2022-12-21 15:37:02 -0300

    primeros pasos

--- src/Area.java
+++ src/Area.java
@@ -1,26 +1,37 @@
 import javax.swing.*;
+
+import java.awt.HeadlessException;
 import java.util.*;
 public class Area {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        System.out.println("Elija una opcion: \n1:Cuadrado \n2:Rectangulo \n3:Triangulo \n4:Circulo");
-        int figura= entrada.nextInt();
-        switch (figura) {
-            case 1:
-                int lado=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del lado en centimetros "));
-                System.out.println("el valor del area es "+Math.pow(lado,2)+" cm");
-                break;
-            case 2:
-                   int base=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la base en centimetros "));
-                   int altura=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la altura en centimetros "));
-                System.out.println("El valor del area es "+base*altura);
-                break;
-            case 3:
-                     base=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la base en centimetros "));
-                     altura=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la altura en centimetros "));
-                     System.out.println("El valor del area es "+(base*altura)/2);
-                     break;
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("Elija una opcion: \n1:Cuadrado \n2:Rectangulo \n3:Triangulo \n4:Circulo");
+            int figura= entrada.nextInt();
+            switch (figura) {
+                case 1:
+                    int lado=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del lado en centimetros "));
+                    System.out.println("el valor del area es "+Math.pow(lado,2)+" cm");
+                    break;
+                case 2:
+                       int base=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la base en centimetros "));
+                       int altura=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la altura en centimetros "));
+                    System.out.println("El valor del area es "+base*altura);
+                    break;
+                case 3:
+                         base=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la base en centimetros "));
+                         altura=Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de la altura en centimetros "));
+                         System.out.println("El valor del area es "+(base*altura)/2);
+                         break;
+                case 4:
+                     int radio=   Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor del radio en centimetros "));
+                    System.out.print("El valor del area del circulo es ");
+                    System.out.println(Math.PI*(Math.pow(radio, 2)));
+                    break;
 
+            }
+        } catch (NumberFormatException | HeadlessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
 
--- src/condicional2.java
+++ src/condicional2.java
@@ -2,23 +2,23 @@
 import java.util.*;
 public class condicional2 {
     public static void main(String[] args) {
-        Scanner entrada= new Scanner(System.in);
+        try (Scanner entrada = new Scanner(System.in)) {
+            System.out.println("ingrese su edad por favor ");
 
-        System.out.println("ingrese su edad por favor ");
-
-        int edad=entrada.nextInt();
-        if (edad<18){
-            System.out.println("eres adolecente");
-            
-        } else if (edad<40) {
-            System.out.println("eres joven");
-            
-        } else if (edad<65) {
-            System.out.println("eres una persona madura");
-            
-        } else  {
-            System.out.println("Cuidate ");
-            
+            int edad=entrada.nextInt();
+            if (edad<18){
+                System.out.println("eres adolecente");
+                
+            } else if (edad<40) {
+                System.out.println("eres joven");
+                
+            } else if (edad<65) {
+                System.out.println("eres una persona madura");
+                
+            } else  {
+                System.out.println("Cuidate ");
+                
+            }
         }
     }
 }
