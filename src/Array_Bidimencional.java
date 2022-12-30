aa6090ff7b5e4276629f3adfc8a10e0f280feb47
Emmanuel Delicia <emmanuel_delicia@hotmail.com>
2022-12-30 18:21:47 -0300

    array bidimencional 1

---
+++ src/Array_Bidimencional.java
@@ -0,0 +1,43 @@
+public class Array_Bidimencional {
+    public static void main(String[] args) {
+        int [][] matrix=new int[4][5];
+
+       matrix[0][0]=64;
+       matrix[0][1]=34;
+       matrix[0][2]=36;
+       matrix[0][3]=57;
+       matrix[0][4]=93;
+
+       matrix[1][0]=86;
+       matrix[1][1]=51;
+       matrix[1][2]=16;
+       matrix[1][3]=10;
+       matrix[1][4]=55;
+
+       matrix[2][0]=23;
+       matrix[2][1]=59;
+       matrix[2][2]=40;
+       matrix[2][3]=92;
+       matrix[2][4]=40;
+
+       matrix[3][0]=63;
+       matrix[3][1]=12;
+       matrix[3][2]=94;
+       matrix[3][3]=16;
+       matrix[3][4]=17;
+
+       for(int i=0;i<4;i++){
+        System.out.println();// se deja una linea vacia para que ciando salte de una pocicion a otra no esten todos los numeros juntos 
+        for(int z=0;z<5;z++){
+            System.out.print(matrix[i][z]+" ");//se deja un espacio vacio para que no esten pegados los numeros 
+        }//al resultado final se ve como una tabla 
+       }
+       
+        
+        
+        
+       }
+
+    }
+    
+}
--- src/uso_array4.java
+++ src/uso_array4.java
@@ -1,7 +1,7 @@
 public class uso_array4 {
     public static void main(String[] args) {
         //rellenar un array con numeros aleatorios 
-        int [] numeros_aleatorios=new int[100];
+        int [] numeros_aleatorios=new int[20];
         for(int i=0;i<numeros_aleatorios.length;i++){
             numeros_aleatorios[i]=(int)Math.round(Math.random()*100);
         }
@@ -9,6 +9,6 @@ public class uso_array4 {
             System.out.print(numeros+",");
             
         }
-    }
+    }// video 27
     
 }
