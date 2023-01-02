655d69b208885c16d5982323943b39980048705d
Emmanuel Delicia <emmanuel_delicia@hotmail.com>
2023-01-02 20:16:46 -0300

    aprendiendo poo

---
+++ src/poo/Coche.java
@@ -0,0 +1,20 @@
+package poo;
+
+public class Coche {
+
+    int ruedas;
+    int peso;
+    int motor;
+    int largo;
+    int ancho;
+
+    public Coche(){
+     ruedas=4;
+     peso=600;
+     motor=1600;
+     largo=2000;
+    ancho=300;
+
+    }
+    
+}
---
+++ src/poo/Uso_Coche.java
@@ -0,0 +1,10 @@
+package poo;
+
+public class Uso_Coche {
+    public static void main(String[] args) {
+        Coche renault= new Coche();
+        System.out.println("este vehiculo posee "+renault.ruedas+ " ruedas");
+    }
+    
+}
+//video clase 30
