3b5f6665a7f102c1bb0cbaca34b0e6a70b7cac29
Emmanuel Delicia <emmanuel_delicia@hotmail.com>
2023-01-06 21:23:24 -0300

    setters getters3

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -1,4 +1,7 @@
 package poo;
+
+import javax.swing.plaf.TreeUI;
+
 public class Coche {
 
    private int ruedas;
@@ -23,15 +26,15 @@ public class Coche {
         return "La plataforma del vehiculo tiene  "+ruedas+" ruedas"+" y mide  "+largo/1000+ " metros con un ancho de "+ancho+
         " cm y un peso  de plataforma de "+peso_plataforma+ " kg y posee un motor de "+motor+" cm cubicos";
     }
-    public void setcolor (String dime_color){
+    public void setcolor (String dime_color){//setter
         color=dime_color;
     }
-    public String getcolor(){
+    public String getcolor(){//getter
         return color;
     }
-    public void configura_asientos(String asientos_cuero){
-        if(asientos_cuero=="si"){
-        this.asientos_cuero=true;}
+    public void configura_asientos(String asientos_cuero){//setter
+        if(asientos_cuero.equalsIgnoreCase("si")){
+        this.asientos_cuero=true;}// this hace enfacis en recalcar que es la variable original no el atributo del metodo
         else{
             this.asientos_cuero=false;
         }
@@ -43,5 +46,43 @@ public class Coche {
             return "el coche tiene asientos  de serie ";
         }
     }
+    public void configura_climatizador(String climatizador){//setter
+        if(climatizador.equalsIgnoreCase("si")){
+            this.climatizador=true;
+
+        }else{ this.climatizador=false;}
+    }
+    public String dime_climatizado(){
+        if (climatizador==true){
+            return "el vehiculo posee climatizador";
+        }else{ return"el vehiculo posee aire de serie";}
+        
+    }
+    public String dime_peso_coche(){//se le ingresan datos y devuelve datos SETTER GETTER
+        int peso_carroceria=500;
+        peso_total=peso_carroceria+peso_plataforma;
+        if (asientos_cuero==true){
+            peso_total=peso_total+50;
+        }
+        if (climatizador==true){
+        peso_total=peso_total+20;
+        }
+        return" El peso total del vehiculo es "+peso_total+" kg";
+
+    }
+    public int precio_coche(){//getters
+        int precio_final=1000;
+        if (asientos_cuero==true){
+            precio_final+=2000;
+        }
+        if (climatizador==true){
+            precio_final+=1500;
+        } return precio_final;
+
+        
+    }
+
+
+
     
 }
--- src/poo/Uso_Coche.java
+++ src/poo/Uso_Coche.java
@@ -1,14 +1,19 @@
 package poo;
-
+import javax.swing.*;
 public class Uso_Coche {
     public static void main(String[] args) {
         Coche renault= new Coche();
-        renault.setcolor("rojo");
+        renault.setcolor(JOptionPane.showInputDialog("introduzca un color "));
         System.out.println("el color del coche es "+renault.getcolor() );
         System.out.println(renault.dime_datos_generales());
-        renault.configura_asientos("no");
+        renault.configura_asientos(JOptionPane.showInputDialog("asientos de cuero si o no"));
         System.out.println(renault.dime_asientos());
+        renault.configura_climatizador(JOptionPane.showInputDialog("climatizador si o no"));
+        System.out.println("el auto posee"+renault.dime_climatizado());
+       System.out.println("el peso total del vehiculo es "+renault.dime_peso_coche()+" kg");
+        System.out.println("el precio del coche es de $ "+renault.precio_coche());
     }
-    
 }
-//video clase 30
+    
+
+
