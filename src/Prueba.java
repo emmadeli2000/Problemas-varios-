554ec4d0c66d36ba0819c9e0629ea7fdbc87312a
Emmanuel Delicia <emmanuel_delicia@hotmail.com>
2023-01-19 20:15:31 -0300

    prueba con uso de static y private

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,10 +2,13 @@ public class Prueba {
     public static void main(String[] args) {
         Empleado empleado1= new Empleado("Paco");
         Empleado empleado2= new Empleado("Ana");
+            empleado1.cambiaSeccion("recursos humanos ");
+        System.out.println(empleado1.dimeDatos());
+        System.out.println(empleado2.dimeDatos());
         
     }
      static class Empleado{ //tuve que llamarla static para poder usar el objeto en el main en el curso no lo dice 
-        String Nombre;
+       private final String Nombre; // final para hacer la palabra nombre una constante en el constructor 
         String Seccion;
         
        public  Empleado(String nom){
