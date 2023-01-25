0e3dd2b15e961c9af5789b0477e19a98fb9e0d47
Emmanuel Delicia <emmanuel_delicia@hotmail.com>
2023-01-25 01:00:38 -0300

    uso de static

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,25 +3,35 @@ public class Prueba {
         Empleado empleado1= new Empleado("Paco");
         Empleado empleado2= new Empleado("Ana");
             empleado1.cambiaSeccion("recursos humanos ");
-        System.out.println(empleado1.dimeDatos());
-        System.out.println(empleado2.dimeDatos());
+        System.out.println(empleado1.dimeDatos()+"\n"+ empleado2.dimeDatos());
+        System.out.println(Empleado.dimeIDsiguiente());
         
     }
      static class Empleado{ //tuve que llamarla static para poder usar el objeto en el main en el curso no lo dice 
        private final String Nombre; // final para hacer la palabra nombre una constante en el constructor 
-        String Seccion;
+        private String Seccion;
+        private int id;// es una variable de clase
+        private static int idSiguente=1; 
+
+        
         
        public  Empleado(String nom){
             Nombre=nom;
             Seccion="Administracion";
+            id=idSiguente;
+            idSiguente++;
 
+             
 
         }
         public void cambiaSeccion(String Seccion){
             this.Seccion=Seccion;
         }
         public String dimeDatos(){
-            return "El nombre del empleado es: "+Nombre+" y la seccion en la que trabaja es: "+Seccion;
+            return "El nombre del empleado es: "+Nombre+" y la seccion en la que trabaja es: "+Seccion+ " y su ID es "+id; 
         }
+        public static String dimeIDsiguiente(){
+            return "el ID siguente es "+idSiguente;
+         }
     }
-}
+} //video 41
